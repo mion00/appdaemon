b8ae9f27074afb83f068e8fb2102664504af32b8
Carlo Mion <mion00@gmail.com>
2023-03-01 09:10:05 +0100

    build(version.py): dinamycally read package version using setuptools_scm
    Resolve package version at runtime using importlib.metatadata.version()

--- appdaemon/version.py
+++ appdaemon/version.py
@@ -1,1 +1,13 @@
-__version__ = "4.3.0"
+# Conditional import to support Python 3.7
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # for Python<3.8
+    from importlib_metadata import version, PackageNotFoundError
+
+# Retrieve package version using metadata from the package itself
+# https://pypi.org/project/setuptools-scm/
+try:
+    __version__ = version("appdaemon")
+except PackageNotFoundError:
+    # package is not installed
+    pass
