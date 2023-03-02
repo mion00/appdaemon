9cacfed318b660d416cbf195aea4c9ef264b8c6a
Carlo Mion <mion00@gmail.com>
2023-03-02 21:56:37 +0100

    ci(python-tests.yml): install the package in editable mode for the unit tests

--- appdaemon/version.py
+++ appdaemon/version.py
@@ -6,6 +6,7 @@ except ImportError:  # for Python<3.8
 
 # Retrieve package version using metadata from the package itself
 # https://pypi.org/project/setuptools-scm/
+# For this to work in a local environment, the package must be installed via `pip install -e .`
 try:
     __version__ = version("appdaemon")
 except PackageNotFoundError:
